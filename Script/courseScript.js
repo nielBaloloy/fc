9803ddea5ee6e1538c3723d778c2780569abd6d8
nielBaloloy <baloloycrisniel@gmail.com>
2024-01-09 19:11:02 +0800

    "add course"

---
+++ Script/courseScript.js
@@ -0,0 +1,9 @@
+
+let showCollege =()=>{
+    document.getElementById('collegeCOURSE').style.display='block';
+    document.getElementById('shsCOURSE').style.display='none';
+}
+let showSHS =()=>{
+    document.getElementById('collegeCOURSE').style.display='none';
+    document.getElementById('shsCOURSE').style.display='block';
+}
